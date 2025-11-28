ad7db7aa9b88b4b17695fac729472e5bbea80688
mohamediiii12 <mohamedalielabd@gmail.com>
2025-11-29 01:54:19 +0200

    fix

--- src/app/components/Home/home.component.ts
+++ src/app/components/Home/home.component.ts
@@ -7,4 +7,25 @@ import { RouterModule } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  ngAfterViewInit() {
+  const counters = document.querySelectorAll('.stat-number');
+
+  counters.forEach((counter: any) => {
+    const updateCount = () => {
+      const target = +counter.getAttribute('data-target');
+      const current = +counter.innerText;
+      const increment = target / 150; 
+
+      if (current < target) {
+        counter.innerText = Math.ceil(current + increment);
+        setTimeout(updateCount, 20);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCount();
+  });
+}
+}
